c6eda36e8bc3a7e3455f1c347f4ace9d2dbae2e2
dario <cochocossolidario@gmail.com>
2023-07-08 15:57:19 -0300

    main actualizado

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import App from "./App.vue"
+import VueAxios from "vue-axios"
+import axios from 'axios'
 
-createApp(App).mount('#app')
+//import router from "./router";
+
+
+const app = createApp(App)
+app.use(VueAxios, axios) 
+
+
+app.mount('#app')
