e8626d42c2135eb95bc53a7ab428120427ef646e
dario <cochocossolidario@gmail.com>
2024-02-04 01:29:40 -0300

    04/02/2024

--- frontend/src/components/Autor/Listar.vue
+++ frontend/src/components/Autor/Listar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Listar Autores</h1>
-    <router-link to="/Autor/crear" class="crear">
+    <router-link to="/Autor/crear" class="btn-crear">
       <img src="../../assets/editar.svg" alt="" />Crear Autores
     </router-link>
 
@@ -18,8 +18,8 @@
           <td>{{ autor.id }}</td>
           <td>{{ autor.nombre_apellido }}</td>
           <td>
-            <button @click="editarAutor(autor.id)" class="btn editar">Editar</button>
-            <button @click="eliminarAutorConfirmacion(autor.id)" class="btn eliminar">Eliminar</button>
+            <button @click="editarAutor(autor.id)" class="btn-editar">Editar</button>
+            <button @click="eliminarAutorConfirmacion(autor)" class="btn-eliminar">Eliminar</button>
           </td>
         </tr>
       </tbody>
@@ -49,24 +49,19 @@ export default {
       }
     },
     async editarAutor(id) {
-      // Redirigir a la página de edición del autor con el ID proporcionado
       this.$router.push(`/Autor/Actualizar/${id}`);
     },
     async eliminarAutorConfirmacion(autor) {
       if (confirm(`¿Estás seguro de que quieres eliminar al Autor ${autor.id}?`)) {
-        await this.eliminarAutor(autor);
+        await this.eliminarAutor(autor.id);
       }
     },
-
     async eliminarAutor(id) {
       try {
-        const res = await axios.delete(`http://192.168.20.10/apiv1/autores/${id}`);
-        console.log(res.data); // Mostrar mensaje de éxito si es necesario
-        // Eliminar el autor del array items
-        this.items = this.items.filter(item => item.id !== id);
+        await axios.delete(`http://192.168.20.10/apiv1/autores/${id}`);
+        this.items = this.items.filter(autor => autor.id !== id);
       } catch (error) {
         console.error(error);
-        // Mostrar mensaje de error si es necesario
       }
     }
   }
@@ -74,50 +69,52 @@ export default {
 </script>
 
 <style scoped>
-table {
-  background: #fff;
-  border-radius: 10px;
-  inset: 10px 50px 10px;
-  box-shadow: 5px 5px 50px rgb(4, 96, 0.2);
-  width: 100%;
-  color: black;
-  margin: 20px;
-  padding: 15px;
-}
-thead {
-  background-color: #f2f2f2;
-}
-th {
-  padding: 10px;
-}
-.btn {
-  padding: 8px 12px;
-  border-radius: 5px;
-  cursor: pointer;
-  font-weight: bold;
-}
-
-/* Estilo para el botón "Editar" */
-.btn.editar {
-  background-color: #3498db; /* Azul */
-  color: #fff;
-}
-
-/* Estilo para el botón "Eliminar" */
-.btn.eliminar {
-  background-color: #e74c3c; /* Rojo */
-  color: #fff;
-}
-
-/* Estilo para el botón "Crear" */
-.crear {
+.btn-crear {
   display: inline-block;
   padding: 8px 12px;
   border-radius: 5px;
   background-color: #27ae60; /* Verde */
   color: #fff;
   text-decoration: none;
+  margin-bottom: 10px;
+}
+
+.btn-editar,
+.btn-eliminar {
+  padding: 8px 12px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-weight: bold;
+  margin-right: 5px;
+}
+
+.btn-editar {
+  background-color: #3498db; /* Azul */
+  color: #fff;
+}
+
+.btn-eliminar {
+  background-color: #e74c3c; /* Rojo */
+  color: #fff;
+}
+
+table {
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 5px 5px 50px rgba(4, 96, 0, 0.2);
+  width: 100%;
+  color: black;
+  margin-bottom: 20px;
+  padding: 15px;
+}
+
+th {
+  background-color: #f2f2f2;
+  padding: 10px;
+}
+
+th, td {
+  text-align: left;
+  padding: 10px;
 }
 </style>
-
-  
