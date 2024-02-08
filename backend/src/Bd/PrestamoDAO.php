8968b8733244e70d8082116983d229c280122418
dario <cochocossolidario@gmail.com>
2024-02-08 19:53:39 -0300

    actu

--- backend/src/Bd/PrestamoDAO.php
+++ backend/src/Bd/PrestamoDAO.php
@@ -5,62 +5,57 @@ namespace Raiz\Bd;
 use Raiz\Aux\Serializador;
 use Raiz\Bd\InterfaceDAO;
 use Raiz\Models\Prestamo;
-use Raiz\Models\Socio;
-
 
 class PrestamoDAO implements InterfaceDAO
 {
-
     public static function listar(): array
     {
         $sql = 'SELECT * FROM prestamos';
-        $listaPrestamo = ConectarBD::leer(sql: $sql);
-        $prestamo = [];
-        foreach ($listaPrestamo as $prestamos) {
-            $prestamo[] = Prestamo::deserializar($prestamos);
+        $listaPrestamos = ConectarBD::leer($sql);
+        $prestamos = [];
+        foreach ($listaPrestamos as $prestamoData) {
+            $prestamos[] = Prestamo::deserializar($prestamoData);
         }
-        return $prestamo;
+        return $prestamos;
     }
+
     public static function encontrarUno(string $id): ?Prestamo
     {
-        $sql = 'SELECT * FROM prestamos WHERE id =:id;';
-        $prestamo = ConectarBD::leer(sql: $sql, params: [':id' => $id]);
-        if (count($prestamo) === 0) {
-           return null;
+        $sql = 'SELECT * FROM prestamos WHERE id = :id';
+        $prestamoData = ConectarBD::leer($sql, [':id' => $id]);
+        if (empty($prestamoData)) {
+            return null; // Devuelve null si no se encuentra ningún préstamo
         } else {
-            $prestamo= Prestamo::deserializar($prestamo[0]);
-            return $prestamo;
+            return Prestamo::deserializar($prestamoData[0]);
         }
     }
 
     public static function crear(Serializador $instancia): void
     {
         $params = $instancia->serializar();
-        $sql = 'INSERT INTO prestamos (socio, libro) VALUES (:socio, :libro)';
-        ConectarBD::escribir(
-            sql: $sql,
-            params: [
-                ':socio' => $params['socio'],
-                ':libro' => $params['libro']
-            ]
-        );
+        $sql = 'INSERT INTO prestamos (libro_id, fecha_inicio, fecha_fin) VALUES (:libro_id, :fecha_inicio, :fecha_fin)';
+        ConectarBD::escribir($sql, [
+            ':libro_id' => $params['libro_id'],
+            ':fecha_inicio' => $params['fecha_inicio'],
+            ':fecha_fin' => $params['fecha_fin']
+        ]);
     }
 
     public static function actualizar(Serializador $instancia): void
     {
         $params = $instancia->serializar();
-        $sql = 'UPDATE Prestamos SET Prestamos =:Prestamos WHERE id=:id';
-        ConectarBD::escribir(
-            sql: $sql,
-            params: [
-                ':id' => $params['id'],
-                ':socio' => $params['socio'],
-            ]
-        );
+        $sql = 'UPDATE prestamos SET libro_id = :libro_id, fecha_inicio = :fecha_inicio, fecha_fin = :fecha_fin WHERE id = :id';
+        ConectarBD::escribir($sql, [
+            ':id' => $params['id'],
+            ':libro_id' => $params['libro_id'],
+            ':fecha_inicio' => $params['fecha_inicio'],
+            ':fecha_fin' => $params['fecha_fin']
+        ]);
     }
+
     public static function borrar(string $id)
     {
-        $sql = 'DELETE FROM prestamos WHERE id=:id';
-        ConectarBD::escribir(sql: $sql, params: [':id' => $id]);
+        $sql = 'DELETE FROM prestamos WHERE id = :id';
+        ConectarBD::escribir($sql, [':id' => $id]);
     }
 }
--- backend/src/Models/Prestamo.php
+++ backend/src/Models/Prestamo.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Raiz\Models;
 
+
 use PhpParser\Node\Stmt\Static_;
 
 class Prestamo extends ModelBase 
